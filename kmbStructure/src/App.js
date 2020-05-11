e0621c99890cb58b1d52181c332954772cb970e1
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-05-11 21:34:54 +0300

    react lazy component update

--- kmbStructure/src/App.js
+++ kmbStructure/src/App.js
@@ -2,16 +2,13 @@ import React, {Component} from "react";
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
-// import ProfileContainer from './components/Profile/Profile';
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
 import Login from './components/Login/Login';
 import Music from './components/Music/Music';
-import MyNews from './components/News/MyNews';
+import ProfileContainer from './components/Profile/Profile';
+import UsersContainer from "./components/Users/UsersContainer";
 import Settings from './components/Settings/Settings';
 import Footer from "./components/Footer/Footer";
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import {connect} from "react-redux";
 import {getAuthUserData, logout, setAuthUserData} from "./redux/auth-reducer";
 import mapStateToProps from "react-redux/src/connect/mapStateToProps";
@@ -19,11 +16,22 @@ import {compose} from "redux";
 import {initializedApp} from "./redux/app-reducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import MyNews from './components/News/MyNews';
+
+const DialogsContainer = React.lazy ( ()=> import ("./components/Dialogs/DialogsContainer"));
+const MyNews = React.lazy ( ()=> import ('./components/News/MyNews') );
+
+
+
+
+
 
 class App extends Component {
        componentDidMount() {
            this.props.initializedApp();
       };
+
 
     render() {
         if (!this.props.initialized) {
@@ -40,17 +48,25 @@ class App extends Component {
 
                     <Route path='/Profile/:userId?' render={() => <ProfileContainer store={this.props.store}/>}/>
 
-                    <Route path='/Dialogs' render={() => <DialogsContainer store={this.props.store}/>}/>
+                    <Route path='/Dialogs' render={() => {
+                             return <React.Suspense fallback={<div>Loading....</div>}>
+                                <DialogsContainer store={this.props.store}/>
+                                    </React.Suspense>    }}/>
+
+                    <Route path='/News' render={() => {
+                             return <React.Suspense fallback={<div>Loading....</div>}>
+                                 <MyNews/>
+                             </React.Suspense>    }}/>
 
                     <Route path='/Users' render={() => <UsersContainer/>}/>
-
-                    <Route path='/News' component={MyNews}/>
 
                     <Route path='/Music' component={Music}/>
 
                     <Route path='/Settings' component={Settings}/>
 
                     <Route path='/Login' component={Login}/>
+
+
 
                 </div>
 
